50b2502f5fe88a56ec8fa8795ae12e436dcdc4d5
Mayowa Ajiboye <64916584+mayowa-aji@users.noreply.github.com>
2025-01-22 21:38:20 -0500

    solution

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+function saturdayFun(activity = 'roller-skate') {
+  return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork(work = 'go to the office') {
+  return `This Monday, I will ${work}.`;
+}
+
+function wrapAdjective(style = '*') {
+  return function inner(adjective = 'special') {
+    return `You are ${style}${adjective}${style}!`;
+  };
+}
+
+
